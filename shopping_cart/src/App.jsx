400914b06a2adf26c43bda86989c0a3effcf236b
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-06-30 16:47:46 -0700

    basic styling

--- shopping_cart/src/App.jsx
+++ shopping_cart/src/App.jsx
@@ -1,20 +1,18 @@
-import "./App.css";
-
 function App() {
   return (
     <>
       <nav>
-        <h1>BearsDen Online Store</h1>
-        <div className="nav-links-div">
+        <h1 className="text-6xl mb-10">BearsDen Online Store</h1>
+        <div className="flex flex-row justify-between">
           <div>
-            <a className="store-link" href="#">
+            <a className="text-2xl ml-2 mr-5" href="#">
               Home
             </a>
-            <a className="store-link" href="#">
+            <a className="text-2xl mr-5" href="#">
               Items for Sale
             </a>
           </div>
-          <a className="store-link" href="#">
+          <a className="text-2xl mr-5" href="#">
             Shopping Chart
           </a>
         </div>
