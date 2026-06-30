2763f8a6236678077c2e72ed96d9b445e688cce4
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-06-30 14:33:07 -0700

    nav links in place

--- shopping_cart/src/App.jsx
+++ shopping_cart/src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <h1>BearsDen Online Store</h1>
+        <div className="nav-links-div">
+          <div>
+            <a className="store-link" href="#">
+              Home
+            </a>
+            <a className="store-link" href="#">
+              Items for Sale
+            </a>
+          </div>
+          <a className="store-link" href="#">
+            Shopping Chart
+          </a>
+        </div>
+        <hr />
+      </nav>
+    </>
+  );
 }
 
 export default App;
