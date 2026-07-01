da6137223ecb1ccf4c357576ab11b742d5ba2b31
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-07-01 16:36:42 -0700

    installed correct tailwindcss version

--- shopping_cart/src/components/ProductCard.jsx
+++ shopping_cart/src/components/ProductCard.jsx
@@ -1,8 +1,10 @@
 const ProductCard = () => {
   return (
-    <div>
-      <img className="" src="81fPKd-2AYL._AC_SL1500_t.png"></img>
-      <h3>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h3>
+    <div className="">
+      <img className="w-3xs" src="81fPKd-2AYL._AC_SL1500_t.png"></img>
+      <h3 className="">
+        Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+      </h3>
       <p>Price: $109.95</p>
       <p>
         Your perfect pack for everyday use and walks in the forest. Stash your
--- shopping_cart/vite.config.js
+++ shopping_cart/vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./tests/setup.js",
-  },
+  plugins: [react(), tailwindcss()],
 });
