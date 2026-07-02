2f80f0661076a5197d44f79103787d8d88a28f2a
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-07-01 17:40:09 -0700

    basic products list set up

--- shopping_cart/src/components/ProductCard.jsx
+++ shopping_cart/src/components/ProductCard.jsx
@@ -1,17 +1,22 @@
 const ProductCard = () => {
   return (
-    <div className="">
-      <img className="w-3xs" src="81fPKd-2AYL._AC_SL1500_t.png"></img>
-      <h3 className="">
-        Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
-      </h3>
-      <p>Price: $109.95</p>
-      <p>
-        Your perfect pack for everyday use and walks in the forest. Stash your
-        laptop (up to 15 inches) in the padded sleeve, your everyday
-      </p>
-      <input type="number"></input>
-      <button>Add to Cart</button>
+    <div className="flex flex-row m-3 w-md h-auto">
+      <img className="w-40 h-fit" src="81fPKd-2AYL._AC_SL1500_t.png"></img>
+      <div className="flex flex-col ml-2">
+        <h3 className="mb-2 text-xl">
+          Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+        </h3>
+        <p className="mb-2">Price: $109.95</p>
+        <p className="mb-5">
+          Your perfect pack for everyday use and walks in the forest. Stash your
+          laptop (up to 15 inches) in the padded sleeve, your everyday needs.
+        </p>
+        <div className="flex flex-row mb-2">
+          <p>Quantity: </p>
+          <input type="number" className=""></input>
+        </div>
+        <button className="align-center mb-2">Add to Cart</button>
+      </div>
     </div>
   );
 };
--- shopping_cart/src/components/ProductsList.jsx
+++ shopping_cart/src/components/ProductsList.jsx
@@ -2,9 +2,28 @@ import ProductCard from "./ProductCard";
 
 const ProductList = () => {
   return (
-    <>
+    <div className="grid grid-cols-3 grid-rows-auto gap-3">
       <ProductCard />
-    </>
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
   );
 };
 
